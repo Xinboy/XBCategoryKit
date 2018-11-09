0c6b0888fb140e16054551c38d4ed9c9771265f9
Xinboy <xinbo.hong@hotmail.com>
2018-11-09 09:29:45 +0800

    first commit

---
+++ Example/Pods/Target Support Files/XBCategoryKit/XBCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XBCategoryKit : NSObject
+@end
+@implementation PodsDummy_XBCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/XBCategoryKit/XBCategoryKit-umbrella.h
@@ -0,0 +1,34 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSData+Encryption.h"
+#import "NSDate+Extension.h"
+#import "NSObject+Invocation.h"
+#import "NSObject+Properties.h"
+#import "NSString+Encryption.h"
+#import "NSString+Regex.h"
+#import "NSString+Utils.h"
+#import "UIColor+Extension.h"
+#import "UIFont+adapt.h"
+#import "UIImage+Extension.h"
+#import "UIAlertController+Extension.h"
+#import "UIButton+Utils.h"
+#import "PlaceholderView.h"
+#import "UIView+CornerRadius.h"
+#import "UIView+Frame.h"
+#import "UIView+placeholderView.h"
+#import "UIViewController+Extention.h"
+#import "UIViewController+Tracking.h"
+
+FOUNDATION_EXPORT double XBCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char XBCategoryKitVersionString[];
+
